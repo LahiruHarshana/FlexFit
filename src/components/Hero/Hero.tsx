66684ab5efa4b4e5801b6c9a5aac9dbe5ee70a16
LahiruHarshana <lharshana2002@gmail.com>
2024-04-13 20:11:18 +0530

    add reponsive for hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import './Hero.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
 const Hero = () => {
   return (
-    <div className='hero col-md-12'>
-      <div className='hero-text'>
-      <h1>GO HARD OR GO HOME</h1>
-      <p>Get fit with our certified trainers. Personalized plans to achieve your goals. Build muscle, improve flexibility, and boost wellness.</p>
-      <button className='btn'>START TRAINING</button>
+    <div className='hero container-fluid'>
+      <div className='row'>
+        <div className='col-lg-10 col-md-10 mx-auto'>
+          <div className='hero-text text-center'>
+            <h1>GO HARD OR GO HOME</h1>
+            <p>Get fit with our certified trainers. Personalized plans to achieve your goals. Build muscle, improve flexibility, and boost wellness.</p>
+            <button className='btn btn-primary'>START TRAINING</button>
+          </div>
+        </div>
       </div>
     </div>
   )

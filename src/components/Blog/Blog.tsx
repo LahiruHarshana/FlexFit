57555677e197bdec048f7b335bb0ce2d78b4769b
LahiruHarshana <lharshana2002@gmail.com>
2024-04-14 01:01:39 +0530

    updated

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Blog = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Container>
+      <h1 style={{textAlign:'center'}}>Our Blog</h1>
+      <Row>
+        <Col>
+        <div>
+          <h1>
+            Blog 1
+          </h1>
+        </div>
+        </Col>
 
-export default Blog
+        <Col>
+        <div>
+          <h1>
+            Blog 2 
+          </h1>
+        </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Blog;

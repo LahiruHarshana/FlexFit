11d749aa1695c3a530831bb6a501955d52032120
LahiruHarshana <lharshana2002@gmail.com>
2024-04-13 20:41:17 +0530

    updated

--- src/components/Story/Story.tsx
+++ src/components/Story/Story.tsx
@@ -5,14 +5,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Story = () => {
   return (
     <div className='container'>
-      <div className='row col-lg-12'>
-        <div className='img col-lg-6'>
+      <div className='row'>
+        <div className='col-lg-6 col-md-12'>
+          <div className='img'>
+          </div>
         </div>
-        <div className='text col-lg-6'>
-          <div className='story-text'>
-            <h1>OUR STORY</h1>
-            <p>Our mission is to provide an affordable, high-quality gym experience that motivates our clients to achieve their fitness goals. We believe that everyone should have the opportunity to live a healthy life, and we are committed to making this a reality for all our members.</p>
-            <button className='btn'>READ MORE</button>
+        <div className='col-lg-6 col-md-12'>
+          <div className='text'>
+            <div className='story-text'>
+              <h1>OUR STORY</h1>
+              <p>Our mission is to provide an affordable, high-quality gym experience that motivates our clients to achieve their fitness goals. We believe that everyone should have the opportunity to live a healthy life, and we are committed to making this a reality for all our members.</p>
+              <button className='btn btn-primary'>READ MORE</button>
+            </div>
           </div>
         </div>
       </div>

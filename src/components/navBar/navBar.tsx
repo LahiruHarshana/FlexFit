6dda662de4da645cd09a379ef7b23b37fe2c58c0
LahiruHarshana <lharshana2002@gmail.com>
2024-04-14 22:33:39 +0530

    updted

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -14,9 +14,9 @@ const NavBar = () => {
         <Nav className="mr-auto">
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="#about">About Us</Nav.Link>
-          <Nav.Link href="#">Programs</Nav.Link>
-          <Nav.Link href="#">Gallery</Nav.Link>
-          <Nav.Link style={{marginRight:'10px'}} href="#">Blog</Nav.Link>
+          <Nav.Link href="#service">Programs</Nav.Link>
+          <Nav.Link href="#gallery">Gallery</Nav.Link>
+          <Nav.Link style={{marginRight:'10px'}} href="#blog">Blog</Nav.Link>
         </Nav>
         <Button style={{marginRight:'20px'}} variant="outline-primary" className="btn">CONTACT</Button>
       </Navbar.Collapse>

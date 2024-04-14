b69371d7053f00375a896d6903e13dd49a7a8337
LahiruHarshana <lharshana2002@gmail.com>
2024-04-14 23:02:01 +0530

    updated

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -4,17 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const Services = () => {
-  useEffect(() => {
-    const carousel = document.querySelector('.carousel') as HTMLElement;
-    const carouselInstance = new (window as any).bootstrap.Carousel(carousel, {
-      interval: 2000,
-      pause: 'hover',
-      wrap: true 
-    });
-    return () => {
-      carouselInstance.dispose();
-    };
-  }, []);
 
   return (
     <div className='services-background d-flex align-items-center justify-content-center'>
@@ -27,25 +16,15 @@ const Services = () => {
             </section>
 
             <div id="carouselExampleCaptions" className="carousel slide " style={{position:'relative',top:'25%'}}>
-              <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-              </div>
-
               <div className="carousel-inner">
                 <div className="carousel-item active">
                   <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
                     <div className="col">
-                      <div className="card shadow-sm mx-8 my-5">
+                      <div className="card shadow-sm mx-8 my-5 service-card">
                         <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
                           <image href="https://images.unsplash.com/photo-1534438327276-14e5300c3a48?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" height="100%" width="100%" />
                         </svg>
                         <div className="card-body">
-                          <i className="fa-solid fa-star text-warning"></i>
-                          <i className="fa-solid fa-star text-warning"></i>
-                          <i className="fa-solid fa-star text-warning"></i>
-                          <i className="fa-solid fa-star text-warning"></i>
-                          <i className="fa-regular fa-star text-warning"></i>
                           <p className="card-text">Functional Training Area</p>
                           <div className="d-flex justify-content-between align-items-center">
                             <small className=" rounded" style={{ background: "#f65e13 !important",color:"#8a8a8a"}}>
@@ -56,16 +35,11 @@ const Services = () => {
                       </div>
                     </div>
                     <div className="col">
-                      <div className="card shadow-sm mx-8 my-5">
+                      <div className="card shadow-sm mx-8 my-5 service-card">
                         <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
                           <image href="https://hips.hearstapps.com/hmg-prod/images/killing-that-kettlebell-workout-royalty-free-image-637772778-1562688657.jpg?resize=1200:*" height="100%" width="100%" />
                         </svg>
                         <div className="card-body">
-                          <i className="fa-solid fa-star text-warning"></i>
-                          <i className="fa-solid fa-star text-warning"></i>
-                          <i className="fa-solid fa-star text-warning"></i>
-                          <i className="fa-regular fa-star text-warning"></i>
-                          <i className="fa-regular fa-star text-warning"></i>
                           <p className="card-text">Personalized Training Programs</p>
                           <div className="d-flex justify-content-between align-items-center">
                             <small className=" rounded" style={{ background: "#f65e13 !important",color:"#8a8a8a"}}>
@@ -77,16 +51,11 @@ const Services = () => {
                     </div>
 
                     <div className="col">
-                      <div className="card shadow-sm mx-8 my-5">
+                      <div className="card shadow-sm mx-8 my-5 service-card">
                         <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
                           <image href="https://www.mensjournal.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_1400/MTk2MTM2NDIwMTA3NTYwNDUz/shutterstock_570689305.webp" height="100%" width="100%" />
                         </svg>
                         <div className="card-body">
-                          <i className="fa-solid fa-star text-warning"></i>
-                          <i className="fa-solid fa-star text-warning"></i>
-                          <i className="fa-solid fa-star text-warning"></i>
-                          <i className="fa-regular fa-star text-warning"></i>
-                          <i className="fa-regular fa-star text-warning"></i>
                           <p className="card-text">Mind-Body Wellness Workshops</p>
                           <div className="d-flex justify-content-between align-items-center">
                             <small className=" rounded" style={{ background: "#f65e13 !important",color:"#8a8a8a"}}>
@@ -98,9 +67,6 @@ const Services = () => {
                     </div>
                   </div>
                 </div>
-              </div>
-
-              <div className="control-btn">
               </div>
             </div>
           </div>

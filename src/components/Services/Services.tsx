12b57cfd01195546d75392262c99711c6e84fbee
LahiruHarshana <lharshana2002@gmail.com>
2024-04-14 22:48:55 +0530

    updated

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -101,15 +101,6 @@ const Services = () => {
               </div>
 
               <div className="control-btn">
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span className="visually-hidden">Previous</span>
-                </button>
-
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span className="visually-hidden">Next</span>
-                </button>
               </div>
             </div>
           </div>

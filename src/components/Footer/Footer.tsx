e2f27fb216cefc67758fcf4e17ec41ff99aaf205
LahiruHarshana <lharshana2002@gmail.com>
2024-04-15 21:23:44 +0530

    upddate footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import './Footer.scss'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-section">
+          <h2>About Us</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+        <div className="footer-section">
+          <h2>Contact Us</h2>
+          <p>Email: lharshana2002@gmail.com</p>
+          <p>Phone: +94 78 290 2200</p>
+        </div>
+        <div className="footer-section">
+          <h2>Follow Us</h2>
+          <div className="social-icons">
+            <a href="#"><FontAwesomeIcon icon={faFacebook} /></a>
+            <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
+            <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; 2024 Lahiru Harshana. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
